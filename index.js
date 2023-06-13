0319a7e556a19dccdcd8d8dee1e57c38a3e88e87
Lakhvinder <lakhabhayi@gmail.com>
2023-06-13 12:09:33 +0530

    first

---
+++ index.js
@@ -0,0 +1,86 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const Task = require('./models/Task');
+const CORS = require('cors');
+
+const app = express();
+app.use(CORS());
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(bodyParser.json());
+const uri = process.env.ATLAS_URI;
+
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.log(err));
+
+    
+app.get('/ping', (req, res) => {
+  res.send('Pong')
+})
+
+app.get('/api/tasks', (req, res) => {
+  Task.find()
+    .then((tasks) => res.json(tasks))
+    .catch((err) => res.status(500).json({ error: err.message }));
+});
+
+// Create a new task
+app.post('/api/tasks', (req, res) => {
+  const { title, description, status } = req.body;
+
+  const task = new Task({
+    title,
+    description,
+    status,
+  });
+
+  task
+    .save()
+    .then((savedTask) => res.json(savedTask))
+    .catch((err) => res.status(400).json({ error: err.message }));
+});
+
+// Update a task
+app.put('/api/tasks/:id', (req, res) => {
+  const { id } = req.params;
+  const { title, description, status } = req.body;
+
+  Task.findByIdAndUpdate(
+    id,
+    { title, description, status },
+    { new: true, runValidators: true }
+  )
+    .then((updatedTask) => {
+      if (!updatedTask) {
+        return res.status(404).json({ error: 'Task not found' });
+      }
+      res.json(updatedTask);
+    })
+    .catch((err) => res.status(400).json({ error: err.message }));
+});
+
+// Delete a task
+app.delete('/api/tasks/:id', (req, res) => {
+  const { id } = req.params;
+
+  Task.findByIdAndDelete(id)
+    .then((deletedTask) => {
+      if (!deletedTask) {
+        return res.status(404).json({ error: 'Task not found' });
+      }
+      res.json(deletedTask);
+    })
+    .catch((err) => res.status(400).json({ error: err.message }));
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
+})
---
+++ models/Task.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const taskSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: Boolean,
+    default: false,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('Task', taskSchema);
